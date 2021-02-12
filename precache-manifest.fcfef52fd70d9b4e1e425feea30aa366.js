76d671bcefc3b41ecd45646b404fe02280cdb6dc
Andrew Dmitrev <50593870+nidze4ar@users.noreply.github.com>
2021-02-12 09:32:06 +0000

    build
    build

---
+++ precache-manifest.fcfef52fd70d9b4e1e425feea30aa366.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d3970ec00e90084e9039739ce50c12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd960378dfa7c811d571",
+    "url": "/static/css/2.2bf2b1a7.chunk.css"
+  },
+  {
+    "revision": "6772f53c76de5e2d89ce",
+    "url": "/static/css/main.b9475dbc.chunk.css"
+  },
+  {
+    "revision": "fd960378dfa7c811d571",
+    "url": "/static/js/2.c401da80.chunk.js"
+  },
+  {
+    "revision": "6772f53c76de5e2d89ce",
+    "url": "/static/js/main.8d9b0ffe.chunk.js"
+  },
+  {
+    "revision": "1e28d1a765841014055c",
+    "url": "/static/js/runtime-main.502b990b.js"
+  }
+]);
